13dd04279a9ff670bad157478e6f0516299e3529
JohnnyMcDade <johnnyrmcdade@icloud.com>
2026-05-06 09:54:33 -0700

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,7 +1,18 @@
 import subprocess
 import sys
+import threading
 
-p1 = subprocess.Popen([sys.executable, 'polymarket_bot.py'])
-p2 = subprocess.Popen([sys.executable, 'scanner_agent.py'])
-p1.wait()
-p2.wait()
+def run_bot():
+    subprocess.run([sys.executable, 'polymarket_bot.py'])
+
+def run_scanner():
+    subprocess.run([sys.executable, 'scanner_agent.py'])
+
+t1 = threading.Thread(target=run_bot)
+t2 = threading.Thread(target=run_scanner)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
